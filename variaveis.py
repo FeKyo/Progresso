7c8ad3057507cfe5a64e451481263169abf2b1e2
Felipe <kyotuji@yahoo.com>
2024-08-15 14:55:19 -0300

    Update variaveis.py

--- variaveis.py
+++ variaveis.py
@@ -1,12 +1,18 @@
 #print("Hello World")
+
 #nome = "Kyu"
 #Profissão = "FullSail"
 #print(type(nome))
 #print(type(Profissão))
-y = 3
-x = 1
-#print(type(x))
-#print(type(y))
-print(x + y)
-pi = 3.1415
-print(type(pi))
+
+#y = 3
+#x = 1
+##print(type(x))
+##print(type(y))
+#print(x + y)
+
+#pi = 3.1415
+#print(type(pi))
+
+nandoéTop = True
+print(type(nandoéTop))
